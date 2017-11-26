88c98d0396ad09221e669ac499f22e953c7d5b9d
SimonJ <sjg777@gmail.com>
2017-11-25 16:22:19 -0800

    LOG macros

--- app/src/main/cpp/jcj.cpp
+++ app/src/main/cpp/jcj.cpp
@@ -1,6 +1,6 @@
 #include "jcj.h"
 
 float add(float a, float b) {
-    __android_log_print(ANDROID_LOG_VERBOSE, TAG, "%f + %f = %f", a, b, a + b);
+    LOGV("%f + %f = %f", a, b, a + b);
     return a + b;
 }
--- app/src/main/cpp/jcj.h
+++ app/src/main/cpp/jcj.h
@@ -5,6 +5,12 @@
 
 #define TAG "JCJ-C"
 
+#define LOGV(fmt, ...) __android_log_print(ANDROID_LOG_VERBOSE, TAG, fmt, ##__VA_ARGS__)
+#define LOGD(fmt, ...) __android_log_print(ANDROID_LOG_DEBUG, TAG, fmt, ##__VA_ARGS__)
+#define LOGI(fmt, ...) __android_log_print(ANDROID_LOG_INFO, TAG, fmt, ##__VA_ARGS__)
+#define LOGW(fmt, ...) __android_log_print(ANDROID_LOG_WARNING, TAG, fmt, ##__VA_ARGS__)
+#define LOGE(fmt, ...) __android_log_print(ANDROID_LOG_ERROR, TAG, fmt, ##__VA_ARGS__)
+
 extern float add(float a, float b);
 
 #endif // JCJ_H
