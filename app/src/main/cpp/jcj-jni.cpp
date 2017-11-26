58f9b88024602dfef91ba29c3672595b7f541e88
SimonJ <sjg777@gmail.com>
2017-11-25 16:09:19 -0800

    jcj-lib split

--- app/src/main/cpp/jcj-jni.cpp
+++ app/src/main/cpp/jcj-jni.cpp
@@ -1,9 +1,10 @@
 #include <jni.h>
+#include "jcj.h"
 
 extern "C"
 
 JNIEXPORT jfloat JNICALL
 Java_com_example_jcj_MainActivity_add(JNIEnv *env, jobject /* this */ obj, jfloat a, jfloat b)
 {
-    return a + b;
+    return add(a, b);
 }
---
+++ app/src/main/cpp/jcj.h
@@ -0,0 +1,10 @@
+#ifndef JCJ_H
+#define JCJ_H
+
+#include <android/log.h>
+
+#define TAG "JCJ-C"
+
+extern float add(float a, float b);
+
+#endif // JCJ_H
